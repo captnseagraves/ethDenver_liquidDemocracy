cce70e707e62e093272a628b1ba3875f24f3431d
Melissa Wagasky <melissawagasky@gmail.com>
2018-02-18 08:57:31 -0700

    Style remaining

--- src/containers/Count/Count.js
+++ src/containers/Count/Count.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
-import './Count.css'
+import '../../App.css';
 
 class Count extends Component {
   render() {
     return (
-      <div className="Count">
-        <button>Submit my delegate votes</button>
+      <div className="delegateVoteView">
+        <h3>You have: 4 votes</h3>
+        <p className="small">Time remaining: 4hr 2m</p>
+        <p className="small">The count period is almost over!</p>
+        <p>Submit your delegated votes now</p>
+        <button className="cta-button">Submit</button>
       </div>
     );
   }
--- src/containers/FinalView/FinalView.js
+++ src/containers/FinalView/FinalView.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './FinalView.css';
 import Complete from '../Complete/Complete';
 import Counter from '../Counter/Counter';
+import '../../App.css';
+
 
 class FinalView extends Component {
   render() {
     return (
-      <div className="delegateVoteView">
+      <div className="delegateSection2 ">
           <p> 11 community members and 3 delegates voted</p>
 
           <p className="completeMessage"> Congrats! The proposal passed! Thank you for participating in our community!</p>
