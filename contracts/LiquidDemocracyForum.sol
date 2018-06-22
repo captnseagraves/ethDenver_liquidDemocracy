e43fd572b8187cbdea82775cdc0310e20455e7a4
Kevin Seagraves <kevin.seagraves@bcgdv.com>
2018-06-22 14:18:49 -0700

    forum linted

--- contracts/LiquidDemocracyForum.sol
+++ contracts/LiquidDemocracyForum.sol
@@ -32,7 +32,7 @@ contract LiquidDemocracyForum is LDForumInterface, Ownable {
 
     modifier isDelegationExpirationIntervalOpen() {
         require(block.timestamp < delegationExpiration, "Delegation Expiration Interval Closed");
-      _;
+        _;
     }
 
     /*would clean and reduce modifiers and helper functions for production*/
@@ -61,287 +61,289 @@ contract LiquidDemocracyForum is LDForumInterface, Ownable {
         _;
     }
 
-  event newPoll(address _newPollAddress, uint _pollId);
-  event delegationExpirationIntervalReset(uint _delegationExpiration);
-  event newTopicCreated(uint _numberOfValidTopicOptions, bytes32 _newTopicMetaData);
-  event voterRegistered(address _newVoterAddress);
-  event newTopicDelegate(address _newDelegate, uint _topic);
-  event voteDelegatedByTopic(address _voter, uint _topic, address _delegateAddress);
-  event topicDelegationRevoked(address _voter, uint _topic);
+    event newPoll(address _newPollAddress, uint _pollId);
+    event delegationExpirationIntervalReset(uint _delegationExpiration);
+    event newTopicCreated(uint _numberOfValidTopicOptions, bytes32 _newTopicMetaData);
+    event voterRegistered(address _newVoterAddress);
+    event newTopicDelegate(address _newDelegate, uint _topic);
+    event voteDelegatedByTopic(address _voter, uint _topic, address _delegateAddress);
+    event topicDelegationRevoked(address _voter, uint _topic);
 
 
 
 
 
-constructor (uint _validTopicOptions, bytes32 _topicMetaData, uint _delegationDepth, uint _delegationExpirationInDays) public {
-  validTopicOptions = _validTopicOptions;
-  topicMetaData = _topicMetaData;
-  delegationDepth = _delegationDepth;
-  delegationExpiration = block.timestamp + (_delegationExpirationInDays * 1 days);
-  pollId = 0;
-/*Ideally will have multiple stewards for a forum*/
-  owner = msg.sender;
-}
+    constructor (uint _validTopicOptions, bytes32 _topicMetaData, uint _delegationDepth, uint _delegationExpirationInDays) public {
+        validTopicOptions = _validTopicOptions;
+        topicMetaData = _topicMetaData;
+        delegationDepth = _delegationDepth;
+        delegationExpiration = block.timestamp + (_delegationExpirationInDays * 1 days);
+        pollId = 0;
+        /*Ideally will have multiple stewards for a forum*/
+        owner = msg.sender;
+    }
 
-function resetDelegationExpirationInterval(uint _numberOfDays)
-/*anyone can call this function, time lock is sufficient and desireable*/
- external
-{
-  require(block.timestamp > delegationExpiration, "Delegation Expiration Interval is still open");
-  delegationExpiration = block.timestamp + (_numberOfDays * 1 days);
+    function resetDelegationExpirationInterval(uint _numberOfDays)
+    /*anyone can call this function, time lock is sufficient and desireable*/
+    external
+    {
+        require(block.timestamp > delegationExpiration, "Delegation Expiration Interval is still open");
+        delegationExpiration = block.timestamp + (_numberOfDays * 1 days);
 
-  emit delegationExpirationIntervalReset(delegationExpiration);
-}
+        emit delegationExpirationIntervalReset(delegationExpiration);
+    }
 
-function createNewTopic(uint _numberOfValidTopicOptions, bytes32 _newTopicMetaData)
-external
-/*Ideally will have multiple stewards for a forum to allow many users to create topics/polls*/
-onlyOwner
-{
+    function createNewTopic(uint _numberOfValidTopicOptions, bytes32 _newTopicMetaData)
+    external
+    /*Ideally will have multiple stewards for a forum to allow many users to create topics/polls*/
+    onlyOwner
+    {
 
-  validTopicOptions = _numberOfValidTopicOptions;
-  topicMetaData = _newTopicMetaData;
+        validTopicOptions = _numberOfValidTopicOptions;
+        topicMetaData = _newTopicMetaData;
 
-  emit newTopicCreated(_numberOfValidTopicOptions, _newTopicMetaData);
-}
+        emit newTopicCreated(_numberOfValidTopicOptions, _newTopicMetaData);
+    }
 
-function createPoll(
-  uint _delegatePeriodEnd,
-  uint _votePeriodEnd,
-  uint _pctQuorum,
-  uint _pctThreshold,
-  bytes32 _proposalMetaData,
-  uint _validVoteOptions,
-  uint _topic
-  )
-  external
-  isValidTopicOption(_topic)
-  /*Ideally will have multiple stewards for a forum to allow many users to create topics/polls*/
-  onlyOwner
-  returns (address)
-{
+    function createPoll(
+        uint _delegatePeriodEnd,
+        uint _votePeriodEnd,
+        uint _pctQuorum,
+        uint _pctThreshold,
+        bytes32 _proposalMetaData,
+        uint _validVoteOptions,
+        uint _topic
+    )
+    external
+    isValidTopicOption(_topic)
+    /*Ideally will have multiple stewards for a forum to allow many users to create topics/polls*/
+    onlyOwner
+    returns (address)
+    {
 
-  LiquidDemocracyPoll current = new LiquidDemocracyPoll(
-    _delegatePeriodEnd,
-    _votePeriodEnd,
-    delegationDepth,
-    _pctQuorum,
-    _pctThreshold,
-    _proposalMetaData,
-    _validVoteOptions,
-    pollId,
-    this,
-    _topic
-      );
+        LiquidDemocracyPoll current = new LiquidDemocracyPoll(
+        _delegatePeriodEnd,
+        _votePeriodEnd,
+        delegationDepth,
+        _pctQuorum,
+        _pctThreshold,
+        _proposalMetaData,
+        _validVoteOptions,
+        pollId,
+        this,
+        _topic
+        );
 
-  pollList[pollId] = current;
+        pollList[pollId] = current;
 
-  emit newPoll(current, pollId);
+        emit newPoll(current, pollId);
 
-  pollId++;
+        pollId++;
 
-}
+    }
 
-function registerVoter()
-external
-{
+    function registerVoter()
+    external
+    {
 
-  require(registeredVotersMap[msg.sender] == false, "Voter already registered");
+        require(registeredVotersMap[msg.sender] == false, "Voter already registered");
 
-  registeredVotersArray.push(msg.sender);
-  registeredVotersMap[msg.sender] = true;
+        registeredVotersArray.push(msg.sender);
+        registeredVotersMap[msg.sender] = true;
 
-  emit voterRegistered(msg.sender);
-}
+        emit voterRegistered(msg.sender);
+    }
 
-  /*allows user to offer themselves as a delegate*/
-function becomeDelegateForTopic(uint _topic)
-external
-isRegisteredVoter
-isValidTopicOption(_topic)
-isDelegationExpirationIntervalOpen
-{
-  willingtoBeTopicDelegate[delegationExpiration][msg.sender][_topic] = true;
+    /*allows user to offer themselves as a delegate*/
+    function becomeDelegateForTopic(uint _topic)
+    external
+    isRegisteredVoter
+    isValidTopicOption(_topic)
+    isDelegationExpirationIntervalOpen
+    {
+        willingtoBeTopicDelegate[delegationExpiration][msg.sender][_topic] = true;
 
-  emit newTopicDelegate(msg.sender, _topic);
-}
+        emit newTopicDelegate(msg.sender, _topic);
+    }
 
-function delegateVoteForTopic(uint _topic, address _delegateAddress)
-external
-isRegisteredVoter
-isValidDelegateForTopic(_delegateAddress, _topic)
-isValidTopicOption(_topic)
-isValidChainDepthAndNonCircular(_topic)
-isDelegationExpirationIntervalOpen
-{
+    function delegateVoteForTopic(uint _topic, address _delegateAddress)
+    external
+    isRegisteredVoter
+    isValidDelegateForTopic(_delegateAddress, _topic)
+    isValidTopicOption(_topic)
+    isValidChainDepthAndNonCircular(_topic)
+    isDelegationExpirationIntervalOpen
+    {
 
-  delegatesForUser[delegationExpiration][msg.sender][_topic] = _delegateAddress;
+        delegatesForUser[delegationExpiration][msg.sender][_topic] = _delegateAddress;
 
-  emit voteDelegatedByTopic(msg.sender, _topic, _delegateAddress);
-}
+        emit voteDelegatedByTopic(msg.sender, _topic, _delegateAddress);
+    }
 
 
-function revokeDelegationForTopic(uint _topic)
-external
-isRegisteredVoter
-isValidTopicOption(_topic)
-isDelegationExpirationIntervalOpen
-{
+    function revokeDelegationForTopic(uint _topic)
+    external
+    isRegisteredVoter
+    isValidTopicOption(_topic)
+    isDelegationExpirationIntervalOpen
+    {
 
-  delegatesForUser[delegationExpiration][msg.sender][_topic] = 0x0;
+        delegatesForUser[delegationExpiration][msg.sender][_topic] = 0x0;
 
-  emit topicDelegationRevoked(msg.sender, _topic);
-}
+        emit topicDelegationRevoked(msg.sender, _topic);
+    }
 
-function readDelegateForTopic(address _userAddress, uint _topic)
-external
-view
-isDelegationExpirationIntervalOpen
-returns (address _delegateAddress)
-{
-  return delegatesForUser[delegationExpiration][_userAddress][_topic];
-}
+    function readDelegateForTopic(address _userAddress, uint _topic)
+    external
+    view
+    isDelegationExpirationIntervalOpen
+    returns (address _delegateAddress)
+    {
+        return delegatesForUser[delegationExpiration][_userAddress][_topic];
+    }
 
-function readEndDelegateForTopic(address _userAddress, uint _topic, uint _recursionCount)
-public
-view
-isDelegationExpirationIntervalOpen
-returns (address _endDelegateAddress)
-{
+    function readEndDelegateForTopic(address _userAddress, uint _topic, uint _recursionCount)
+    public
+    view
+    isDelegationExpirationIntervalOpen
+    returns (address _endDelegateAddress)
+    {
 
-  if (_recursionCount > delegationDepth){
-   return 0x0;
-  }
-
-  if (delegatesForUser[delegationExpiration][_userAddress][_topic] == 0x0) {
-    return _userAddress;
-  } else {
-    return readEndDelegateForTopic(delegatesForUser[delegationExpiration][_userAddress][_topic], _topic, _recursionCount + 1);
-  }
-}
-
-/* if we can make this a view function, that would be ideal*/
-function finalDecision(address _pollAddress)
-external
-view
-returns (uint _finalDecision, uint _finalDecisionTally)
-{
-
-  uint totalVotes;
-  uint emptyVotes;
-  uint[256] memory _tallyResults;
-
-  (_tallyResults, totalVotes, emptyVotes) = tally(_pollAddress);
-
-uint _pctQuorum = LDPollInterface(_pollAddress).readPctQuorum();
-uint _pctThreshold = LDPollInterface(_pollAddress).readPctThreshold();
-
-  if (registeredVotersArray.length == 0 || (totalVotes * 100) / (registeredVotersArray.length) < _pctQuorum) {
-    _finalDecision = 0;
-    _finalDecisionTally = 0;
-    return;
-  } else {
-
-    uint highestVoteHold = 0;
-    uint highestVoteValueHold = 0;
-
-      for (uint i = 0; i < _tallyResults.length; i++) {
-        if (_tallyResults[i] > highestVoteValueHold) {
-          highestVoteValueHold = _tallyResults[i];
-          highestVoteHold = i;
+        if (_recursionCount > delegationDepth){
+            return 0x0;
         }
-      }
 
-      if (((highestVoteValueHold * 100) / totalVotes) > _pctThreshold) {
-        _finalDecision = highestVoteHold;
-        _finalDecisionTally = highestVoteValueHold;
-        return;
-      } else {
-        _finalDecision = 0;
-        _finalDecisionTally = 0;
-        return;
-      }
-  }
-}
-
-/*allows user tally votes at */
-function tally(address _pollAddress)
-public
-view
-returns (uint[256] _votes, uint _totalVotes, uint _emptyVotes)
-{
-
-  /* address forumDelegate = LDForumInterface(forumAddress).readEndDelegateForTopic(_userAddress, topic, 0); */
-
-
-  //todo: how to handle vote validation and initialization
-  for (uint i = 0; i < registeredVotersArray.length; i++){
-    uint vote;
-  (vote,) = LDPollInterface(_pollAddress).readVoteAndEndVoter(registeredVotersArray[i], 0);
-    _votes[vote]++;
-
-    if(vote > 0){
-        _totalVotes++;
-    } else {
-        _emptyVotes++;
+        if (delegatesForUser[delegationExpiration][_userAddress][_topic] == 0x0) {
+            return _userAddress;
+        } else {
+            return readEndDelegateForTopic(delegatesForUser[delegationExpiration][_userAddress][_topic], _topic, _recursionCount + 1);
+        }
     }
-  }
-  return (_votes, _totalVotes, _emptyVotes);
-}
 
+    /* if we can make this a view function, that would be ideal*/
+    function finalDecision(address _pollAddress)
+    external
+    view
+    returns (uint _finalDecision, uint _finalDecisionTally)
+    {
 
-function _isValidChainDepthAndNonCircular(address _userAddress, uint _topic, uint _recursionCount)
-public
-view
-isDelegationExpirationIntervalOpen
-returns(bool _valid, bool _vDepth, bool _vCircle){
+        uint totalVotes;
+        uint emptyVotes;
+        uint[256] memory _tallyResults;
 
-  if(_recursionCount > delegationDepth){
-    _vDepth = true;
-    _valid = false;
-    return;
-  }
+        (_tallyResults, totalVotes, emptyVotes) = tally(_pollAddress);
 
-  if (delegatesForUser[delegationExpiration][_userAddress][_topic] != 0x0) {
-    if (delegatesForUser[delegationExpiration][_userAddress][_topic] == _userAddress) {
-      _valid = false;
-      _vCircle = true;
-      return;
+        uint _pctQuorum = LDPollInterface(_pollAddress).readPctQuorum();
+        uint _pctThreshold = LDPollInterface(_pollAddress).readPctThreshold();
+
+        if (registeredVotersArray.length == 0 || (totalVotes * 100) / (registeredVotersArray.length) < _pctQuorum) {
+            _finalDecision = 0;
+            _finalDecisionTally = 0;
+            return;
+        } else {
+
+            uint highestVoteHold = 0;
+            uint highestVoteValueHold = 0;
+
+            for (uint i = 0; i < _tallyResults.length; i++) {
+                if (_tallyResults[i] > highestVoteValueHold) {
+                    highestVoteValueHold = _tallyResults[i];
+                    highestVoteHold = i;
+                }
+            }
+
+            if (((highestVoteValueHold * 100) / totalVotes) > _pctThreshold) {
+                _finalDecision = highestVoteHold;
+                _finalDecisionTally = highestVoteValueHold;
+                return;
+            } else {
+                _finalDecision = 0;
+                _finalDecisionTally = 0;
+                return;
+            }
+        }
     }
-    return _isValidChainDepthAndNonCircular(delegatesForUser[delegationExpiration][_userAddress][_topic], _topic, _recursionCount + 1);
-  } else {
-    _valid = true;
-    return;
-  }
-}
+
+    /*allows user tally votes at */
+    function tally(address _pollAddress)
+    public
+    view
+    returns (uint[256] _votes, uint _totalVotes, uint _emptyVotes)
+    {
+
+        /* address forumDelegate = LDForumInterface(forumAddress).readEndDelegateForTopic(_userAddress, topic, 0); */
 
 
- function verifyVoter(address _userAddress)
- view
-  public
-  returns (bool _voterRegistration){
-   if (registeredVotersMap[_userAddress] == true) {
-     return true;
-   } else {
-     return false;
-   }
- }
+        //todo: how to handle vote validation and initialization
+        for (uint i = 0; i < registeredVotersArray.length; i++){
+            uint vote;
+            (vote,) = LDPollInterface(_pollAddress).readVoteAndEndVoter(registeredVotersArray[i], 0);
+            _votes[vote]++;
 
- function _isValidDelegateForTopic(address _userAddress, uint _topic)
- view
-  public
-  isDelegationExpirationIntervalOpen
-  returns (bool _delegateStatus){
-   if (willingtoBeTopicDelegate[delegationExpiration][_userAddress][_topic] == true) {
-     return true;
-   } else {
-     return false;
-   }
- }
+            if(vote > 0){
+                _totalVotes++;
+            } else {
+                _emptyVotes++;
+            }
+        }
 
- function () external payable {
-   revert();
- }
+        return (_votes, _totalVotes, _emptyVotes);
+    }
+
+
+    function _isValidChainDepthAndNonCircular(address _userAddress, uint _topic, uint _recursionCount)
+    public
+    view
+    isDelegationExpirationIntervalOpen
+    returns(bool _valid, bool _vDepth, bool _vCircle)
+    {
+
+        if(_recursionCount > delegationDepth){
+            _vDepth = true;
+            _valid = false;
+            return;
+        }
+
+        if (delegatesForUser[delegationExpiration][_userAddress][_topic] != 0x0) {
+            if (delegatesForUser[delegationExpiration][_userAddress][_topic] == _userAddress) {
+                _valid = false;
+                _vCircle = true;
+                return;
+            }
+            return _isValidChainDepthAndNonCircular(delegatesForUser[delegationExpiration][_userAddress][_topic], _topic, _recursionCount + 1);
+        } else {
+            _valid = true;
+            return;
+        }
+    }
+
+
+    function verifyVoter(address _userAddress)
+    view
+    public
+    returns (bool _voterRegistration){
+        if (registeredVotersMap[_userAddress] == true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function _isValidDelegateForTopic(address _userAddress, uint _topic)
+    view
+    public
+    isDelegationExpirationIntervalOpen
+    returns (bool _delegateStatus){
+        if (willingtoBeTopicDelegate[delegationExpiration][_userAddress][_topic] == true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function () external payable {
+        revert();
+    }
 
 
 }
