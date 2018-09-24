d130f9782e0446bedbaf576cc33f2de43008b5ea
Kevin Seagraves <k.s.seagraves@gmail.com>
2018-09-24 13:44:40 -0600

    resolved testing issues. eth-gas-reporter was messing with mocha.

--- truffle.js
+++ truffle.js
@@ -5,12 +5,5 @@ module.exports = {
       port: 8545,
       network_id: "*" // Match any network id
     }
-  },
-  mocha: {
-    reporter: 'eth-gas-reporter',
-    reporterOptions : {
-      currency: 'USD',
-      gasPrice: 21
-    }
   }
 };
